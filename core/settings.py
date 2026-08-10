25b2bb83e47416237fd2758b48d2e28467ea7af9
Arthur Romão <arthurromao@MacBook-Air-de-Arthur.local>
2026-08-10 15:41:31 -0300

    Ajuste final do settings e sincronizacao do Master

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = 'login'
 
